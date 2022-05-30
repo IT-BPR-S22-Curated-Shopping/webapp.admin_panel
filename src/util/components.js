3a955897103f98c13516259db749102387407be5
jnorsted <jacob@norsted.dk>
2022-05-30 09:38:19 +0200

    Minor clean-up

--- src/util/components.js
+++ src/util/components.js
@@ -1,5 +1,3 @@
-export { default as Home } from "../pages/home/Home";
-export { default as About } from "../pages/about/About";
 export { default as Device } from "../pages/device/DevicePage";
 export { default as Location } from "../pages/location/LocationPage";
 export { default as NewLocation } from "../pages/location/NewLocationPage"
